d13f29ad993745c53f24666f273b27fab0217e8a
iftikhar chetoui <115534833+foufa102022@users.noreply.github.com>
2023-11-01 00:16:02 +0100

    Delete Backend/crowdapp directory

--- Backend/crowdapp/src/main/java/com/devops/crowdapp/dao/ProjectCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devops.crowdapp.dao;
-
-import com.devops.crowdapp.entity.ProjectCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "projectCategory", path ="projectCategory")
-public interface ProjectCategoryRepository extends JpaRepository<ProjectCategory, Long> {
-}
--- Backend/crowdapp/src/main/java/com/devops/crowdapp/dao/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devops.crowdapp.dao;
-
-import com.devops.crowdapp.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
